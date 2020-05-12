a9854c040843a773a2bc186fd63c09795005f70c
Devon Reid <fireborn1003@gmail.com>
2020-05-12 15:53:35 +0600

    FT:API Added login route for login functionality
    * Validate login credentials and return jwt or error
    * Added login router and controllers

--- controllers/auth.js
+++ controllers/auth.js
@@ -14,3 +14,23 @@ exports.register = asyncHandler(async (req, res, next) => {
 	const token = user.getSignedJwtToken();
 	res.status(200).json({ success: true, token });
 });
+
+exports.login = asyncHandler(async (req, res, next) => {
+	const { email, password } = req.body;
+
+	// Validate email and password
+	if (!email || !password) return next(new ErrorResponse(`Please provide an email and password`, 400));
+
+	// Check for user
+	const user = await User.findOne({ email }).select('+password');
+
+	if (!user) return next(new ErrorResponse(`Invalid credentials`, 401));
+
+	// Check if pass matches
+	const isMatch = await user.matchPassword(password);
+	if (!isMatch) return next(new ErrorResponse(`Invalid credentials`, 401));
+
+	// Create token
+	const token = user.getSignedJwtToken();
+	res.status(200).json({ success: true, token });
+});
--- models/User.js
+++ models/User.js
@@ -34,7 +34,10 @@ UserSchema.methods.getSignedJwtToken = function() {
 	});
 };
 
-// Encrypt password using bcrypt
+UserSchema.methods.matchPassword = async function(enteredPass) {
+	return await bcrypt.compare(enteredPass, this.password);
+};
+
 UserSchema.pre('save', async function(next) {
 	const salt = await bcrypt.genSalt(10);
 	this.password = await bcrypt.hash(this.password, salt);
