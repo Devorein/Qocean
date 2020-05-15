2d8e3e0eb46182f3b3d408e4c49e42e59af818a0
Devon Reid <fireborn1003@gmail.com>
2020-05-15 22:05:56 +0600

    E:Style Enhanced tertiary block styling
    * FT:Style added text color variables
    * Made grid 3 cols
    * I:Comp added more appropriate classes
    * E:Style Enhanced tertiary block styling

--- src/resources/Quiz/QuizCard.jsx
+++ src/resources/Quiz/QuizCard.jsx
@@ -9,29 +9,40 @@ class QuizCard extends Component {
 			<div className={`card quiz-card ${page}-card ${page}-quiz-card quiz-card-${index} ${page}-quiz-card-${index}`}>
 				<div className="quiz-card-primary card-primary">{item.name}</div>
 
-				<div className="card-secondary quiz-card-secondary quiz-card-secondary-user">
-					<Link to={`/user/${item.user._id}`}>by {item.user.username}</Link>
+				<div className="card-secondary quiz-card-secondary">
+					<div className="card-secondary quiz-card-secondary-item quiz-card-secondary-user">
+						<Link to={`/user/${item.user._id}`}>by {item.user.username}</Link>
+					</div>
+					<div className="card-secondary-item quiz-card-secondary-item quiz-card-secondary-subject">{item.subject}</div>
+					<div className="card-secondary-item quiz-card-secondary-item quiz-card-secondary-tags">
+						{item.tags.map((tag, index) => (
+							<span key={tag} className={`quiz-card-secondary-tags-item`}>
+								{tag}
+							</span>
+						))}
+					</div>
 				</div>
-				<div className="card-secondary quiz-card-secondary quiz-card-secondary-subject">{item.subject}</div>
-				<div className="card-secondary quiz-card-secondary quiz-card-secondary-tags">
-					{item.tags.map((tag, index) => (
-						<span key={tag} className={`quiz-card-secondary-tags-item`}>
-							{tag}
-						</span>
-					))}
-				</div>
-				<div className="quiz-card-tertiary">
+				<div className="card-tertiary quiz-card-tertiary">
 					<div className="quiz-card-tertiary-item quiz-card-tertiary-averageTimeAllocated">
-						Average Quiz Time {item.averageTimeAllocated}
+						<div className="quiz-card-tertiary-item-key">Average Quiz Time</div>
+						<div className="quiz-card-tertiary-item-value">{item.averageTimeAllocated}s</div>
 					</div>
 					<div className="quiz-card-tertiary-item quiz-card-tertiary-averageDifficulty">
-						Average Quiz Difficulty {item.averageDifficulty}
+						<div className="quiz-card-tertiary-item-key">Average Quiz Difficulty</div>
+						<div className="quiz-card-tertiary-item-value">{item.averageDifficulty}</div>
 					</div>
 					<div className="quiz-card-tertiary-item quiz-card-tertiary-questionsCount">
-						Total Questions {item.questionsCount}
+						<div className="quiz-card-tertiary-item-key">Total Questions </div>
+						<div className="quiz-card-tertiary-item-value">{item.questionsCount}</div>
 					</div>
-					<div className="quiz-card-tertiary-item quiz-card-tertiary-createdAt">Created At {item.createdAt}</div>
-					<div className="quiz-card-tertiary-item quiz-card-tertiary-source">Source: {item.source}</div>
+					<div className="quiz-card-tertiary-item quiz-card-tertiary-createdAt">
+						<div className="quiz-card-tertiary-item-key">Created At</div>
+						<div className="quiz-card-tertiary-item-value">{item.createdAt}</div>
+					</div>
+					<div className="quiz-card-tertiary-item quiz-card-tertiary-source">
+						<div className="quiz-card-tertiary-item-key">Source: </div>
+						<div className="quiz-card-tertiary-item-value">{item.source}</div>
+					</div>
 				</div>
 			</div>
 		);
