2f4c65124c04c54cda605a9d8c64f1e9fb243c13
Devon Reid <fireborn1003@gmail.com>
2020-05-17 15:35:04 +0600

    FT:API->auth Logout (reset cookie) router and controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -45,6 +45,20 @@ exports.login = asyncHandler(async (req, res, next) => {
 	sendTokenResponse(user, 200, res);
 });
 
+// @desc     Logout user
+// @route    GET /api/v1/auth/logout
+// @access   Private
+exports.logout = asyncHandler(async (req, res, next) => {
+	res.cookie('token','none',{
+    expires: new Date(Date.now() + 10 * 1000),
+    httpOnly: true
+  })
+  res.status(200).json({
+    success: true,
+    data: {}
+  })
+});
+
 // @desc     Forgot password
 // @route    GET /api/v1/auth/forgotpassword
 // @access   Public
--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { register, login, forgotPassword, resetPassword } = require('../controllers/auth');
+const { register, login,logout, forgotPassword, resetPassword } = require('../controllers/auth');
 const { protect } = require('../middleware/auth');
 const cors = require('cors');
 
 const router = express.Router();
 
+router.get('/logout', logout);
 router.post('/register', register);
 router.post('/login', login);
 router.post('/forgotpassword', protect, forgotPassword);
