6ae2295da2b3d5cb8acdbe93c83ad90636040807
Devon Reid <fireborn1003@gmail.com>
2020-05-17 11:14:08 +0600

    FT:Comp->SignIn sending login data

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const { register, login, forgotPassword, resetPassword } = require('../controllers/auth');
 const { protect } = require('../middleware/auth');
+const cors = require('cors');
 
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
-router.post('/forgotpassword', forgotPassword);
+router.post('/forgotpassword', protect, forgotPassword);
 router.put('/resetpassword/:resetToken', resetPassword);
 
 module.exports = router;
--- src/components/Auth/SignIn.jsx
+++ src/components/Auth/SignIn.jsx
@@ -1,71 +1,84 @@
 import React, { Component } from 'react';
 import { Formik } from 'formik';
+import axios from 'axios';
 
-const SignIn = () => (
-	<Formik
-		initialValues={{ email: '' }}
-		onSubmit={async (values) => {
-			await new Promise((resolve) => setTimeout(resolve, 500));
-			alert(JSON.stringify(values, null, 2));
-		}}
-		handleChange={(value) => {
-			console.log(value);
-		}}
-		validate={(values) => {
-			const errors = {};
-			if (!values.email) {
-				errors.email = 'Required';
-			} else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
-				errors.email = 'Invalid email address';
-			}
-			return errors;
-		}}
-	>
-		{(props) => {
-			const {
-				values,
-				touched,
-				errors,
-				dirty,
-				isSubmitting,
-				handleChange,
-				handleBlur,
-				handleSubmit,
-				handleReset
-			} = props;
-			return (
-				<form onSubmit={handleSubmit}>
-					<label htmlFor="email" style={{ display: 'block' }}>
-						Email
-					</label>
-					<input
-						id="email"
-						placeholder="Enter your email"
-						type="text"
-						value={values.email}
-						onChange={handleChange}
-						onBlur={handleBlur}
-						className={errors.email && touched.email ? 'text-input error' : 'text-input'}
-					/>
-					{errors.email && touched.email && <div className="input-feedback">{errors.email}</div>}
+class SignIn extends Component {
+	submitForm = (values) => {
+		axios
+			.post(`http://localhost:5001/api/v1/auth/login`, {
+				email: values.email,
+				password: values.password
+			})
+			.then((res) => {
+				console.log(res);
+			});
+	};
+	render() {
+		return (
+			<Formik
+				initialValues={{ email: '', password: '' }}
+				onSubmit={this.submitForm}
+				validate={(values) => {
+					const errors = {};
+					if (!values.email) errors.email = 'Required';
+					else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email))
+						errors.email = 'Invalid email address';
+					return errors;
+				}}
+			>
+				{(props) => {
+					const {
+						values,
+						touched,
+						errors,
+						dirty,
+						isSubmitting,
+						handleChange,
+						handleBlur,
+						handleSubmit,
+						handleReset
+					} = props;
+					return (
+						<form onSubmit={handleSubmit}>
+							<label htmlFor="email" style={{ display: 'block' }}>
+								Email
+							</label>
+							<input
+								id="email"
+								placeholder="Enter your email"
+								type="text"
+								value={values.email}
+								onChange={handleChange}
+								onBlur={handleBlur}
+								className={errors.email && touched.email ? 'text-input error' : 'text-input'}
+							/>
+							{errors.email && touched.email && <div className="input-feedback">{errors.email}</div>}
 
-					<label htmlFor="password" style={{ display: 'block' }}>
-						Password
-					</label>
+							<label htmlFor="password" style={{ display: 'block' }}>
+								Password
+							</label>
 
-					<input type="password" name="password" onChange={handleChange} onBlur={handleBlur} value={values.password} />
-					{errors.password && touched.password && <div className="input-feedback">{errors.password}</div>}
+							<input
+								type="password"
+								name="password"
+								onChange={handleChange}
+								onBlur={handleBlur}
+								value={values.password}
+							/>
+							{errors.password && touched.password && <div className="input-feedback">{errors.password}</div>}
 
-					<button type="button" className="outline" onClick={handleReset} disabled={!dirty || isSubmitting}>
-						Reset
-					</button>
-					<button type="submit" disabled={isSubmitting}>
-						Submit
-					</button>
-				</form>
-			);
-		}}
-	</Formik>
-);
+							<button type="button" className="outline" onClick={handleReset} disabled={!dirty || isSubmitting}>
+								Reset
+							</button>
+							<button type="submit" disabled={isSubmitting}>
+								Submit
+							</button>
+						</form>
+					);
+				}}
+			</Formik>
+		);
+	}
+}
 
 export default SignIn;
