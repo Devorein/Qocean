878c3ec9d2c7b1099e84a396caaff4968e14594b
Devon Reid <fireborn1003@gmail.com>
2020-05-18 11:25:07 +0600

    P.Added user navbar item containing menu items
    * E->Navbar image, item and username

--- src/components/Navbar/Auth/NavbarAuth.jsx
+++ src/components/Navbar/Auth/NavbarAuth.jsx
@@ -1,43 +1,72 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment } from 'react';
 import { NavLink, withRouter } from 'react-router-dom';
-import { Button } from '@material-ui/core';
+import { Button, Menu, MenuItem } from '@material-ui/core';
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 
-class NavbarAuth extends Component {
-	logout = () => {
-		const { refetch } = this.props;
+function NavbarAuth({ session, refetch, history }) {
+	const logout = () => {
 		localStorage.removeItem('token');
-		this.props.history.push('/');
+		history.push('/');
 		refetch();
 	};
-	render() {
-		// const { session } = this.props;
-		return (
-			<Fragment>
-				<NavLink className="navbar-link" to="/" exact>
-					Home
-				</NavLink>
-				<NavLink className="navbar-link" to="/create">
-					Create
-				</NavLink>
-				<NavLink className="navbar-link" to="/explore">
-					Explore
-				</NavLink>
-				<NavLink className="navbar-link" to="/self">
-					Self
-				</NavLink>
-				<NavLink className="navbar-link" to="/app">
-					Play
-				</NavLink>
-				<NavLink className="navbar-link" to="/profile">
-					Profile
-				</NavLink>
-				<Button variant="contained" size="medium" startIcon={<ExitToAppIcon />} onClick={this.logout}>
-					Logout
-				</Button>
-			</Fragment>
-		);
-	}
+
+	const [ anchorEl, setAnchorEl ] = React.useState(null);
+
+	const handleClick = (event) => {
+		setAnchorEl(event.currentTarget);
+	};
+
+	const handleClose = () => {
+		setAnchorEl(null);
+	};
+
+	return (
+		<Fragment>
+			<NavLink className="navbar-link" to="/" exact>
+				Home
+			</NavLink>
+			<NavLink className="navbar-link" to="/create">
+				Create
+			</NavLink>
+			<NavLink className="navbar-link" to="/explore">
+				Explore
+			</NavLink>
+			<NavLink className="navbar-link" to="/self">
+				Self
+			</NavLink>
+			<NavLink className="navbar-link" to="/app">
+				Play
+			</NavLink>
+			<div className="navbar-link">
+				<img
+					className="navbar-link-item navbar-link-item--image"
+					src={
+						session.data.data.image ? (
+							session.data.data.image
+						) : (
+							'https://www.teknozeka.com/wp-content/uploads/2020/03/wp-header-logo-24.png'
+						)
+					}
+					alt={'User'}
+				/>
+				<span className="navbar-link-item navbar-link-item--username" onClick={handleClick}>
+					{session.data.data.username}
+				</span>
+			</div>
+			<Menu id="simple-menu" anchorEl={anchorEl} keepMounted open={Boolean(anchorEl)} onClose={handleClose}>
+				<MenuItem onClick={handleClose}>Profile</MenuItem>
+				<MenuItem onClick={handleClose}>Stats</MenuItem>
+				<MenuItem onClick={handleClose}>Logout</MenuItem>
+			</Menu>
+
+			{/* <NavLink className="navbar-link" to="/profile">
+				Profile
+			</NavLink>
+			<Button variant="contained" size="medium" startIcon={<ExitToAppIcon />} onClick={logout}>
+				Logout
+			</Button> */}
+		</Fragment>
+	);
 }
 
 export default withRouter(NavbarAuth);
