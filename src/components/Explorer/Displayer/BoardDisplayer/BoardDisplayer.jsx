75ce080ef8b371c5447fd26cc1d06ab99004f700
Devon Reid <fireborn1003@gmail.com>
2020-06-08 16:48:57 +0600

    P.FT:Comp->BoardDisplayer&GalleryDisplayer
    * Initial Setup and creation

--- src/components/Explorer/Displayer/BoardDisplayer/BoardDisplayer.jsx
+++ src/components/Explorer/Displayer/BoardDisplayer/BoardDisplayer.jsx
@@ -1,9 +1,44 @@
 import React, { Component } from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import CheckboxInput from '../../../Input/Checkbox/CheckboxInput';
+import './BoardDisplayer.scss';
 
 class BoardDisplayer extends Component {
+	renderBoardDisplayer = () => {
+		const { type, data, classes, setChecked } = this.props;
+		return data.map((item, index) => {
+			return (
+				<div className={`BoardDisplayer_item ${classes.BoardDisplayer_item}`} key={item._id}>
+					<CheckboxInput onChange={setChecked.bind(null, index)} checked={item.checked} />
+					<div className="BoardDisplayer_item_container BoardDisplayer_item_container-actions">{item.actions}</div>
+					{[ 'primary', 'secondary', 'tertiary' ].map((key, index) => {
+						return (
+							<div
+								key={`${type}${key}${index}`}
+								className={`BoardDisplayer_item_container BoardDisplayer_item_container-${key}`}
+							>
+								{Object.entries(item[key]).map(([ key, val ], index) => (
+									<span
+										key={`${type}${key}${index}`}
+										className={`BoardDisplayer_item_container_item BoardDisplayer_item_container-${key}_item`}
+									>
+										{val}
+									</span>
+								))}
+							</div>
+						);
+					})}
+				</div>
+			);
+		});
+	};
 	render() {
-		return <div className="BoardDisplayer">BoardDisplayer</div>;
+		return <div className="BoardDisplayer">{this.renderBoardDisplayer()}</div>;
 	}
 }
 
-export default BoardDisplayer;
+export default withStyles((theme) => ({
+	BoardDisplayer_item: {
+		backgroundColor: theme.palette.background.main
+	}
+}))(BoardDisplayer);
--- src/components/Explorer/Displayer/GalleryDisplayer/GalleryDisplayer.jsx
+++ src/components/Explorer/Displayer/GalleryDisplayer/GalleryDisplayer.jsx
@@ -1,9 +1,44 @@
 import React, { Component } from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import CheckboxInput from '../../../Input/Checkbox/CheckboxInput';
+import './GalleryDisplayer.scss';
 
 class GalleryDisplayer extends Component {
+	renderGalleryDisplayer = () => {
+		const { type, data, classes, setChecked } = this.props;
+		return data.map((item, index) => {
+			return (
+				<div className={`GalleryDisplayer_item ${classes.GalleryDisplayer_item}`} key={item._id}>
+					<CheckboxInput onChange={setChecked.bind(null, index)} checked={item.checked} />
+					<div className="GalleryDisplayer_item_container GalleryDisplayer_item_container-actions">{item.actions}</div>
+					{[ 'primary', 'secondary', 'tertiary' ].map((key, index) => {
+						return (
+							<div
+								key={`${type}${key}${index}`}
+								className={`GalleryDisplayer_item_container GalleryDisplayer_item_container-${key}`}
+							>
+								{Object.entries(item[key]).map(([ key, val ], index) => (
+									<span
+										key={`${type}${key}${index}`}
+										className={`GalleryDisplayer_item_container_item GalleryDisplayer_item_container-${key}_item`}
+									>
+										{val}
+									</span>
+								))}
+							</div>
+						);
+					})}
+				</div>
+			);
+		});
+	};
 	render() {
-		return <div className="GalleryDisplayer">GalleryDisplayer</div>;
+		return <div className="GalleryDisplayer">{this.renderGalleryDisplayer()}</div>;
 	}
 }
 
-export default GalleryDisplayer;
+export default withStyles((theme) => ({
+	GalleryDisplayer_item: {
+		backgroundColor: theme.palette.background.main
+	}
+}))(GalleryDisplayer);
