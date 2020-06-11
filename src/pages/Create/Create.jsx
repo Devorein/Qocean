87081fe80a1984c64603c8dfe4d10760c56c80b5
Devon Reid <fireborn1003@gmail.com>
2020-06-11 12:04:26 +0600

    P.I:Comp->Create Refactored to use FormFiller

--- src/pages/Create/Create.jsx
+++ src/pages/Create/Create.jsx
@@ -1,36 +1,26 @@
 import React, { Component } from 'react';
-import FolderForm from '../../resources/Form/FolderForm';
-import QuestionForm from '../../resources/Form/QuestionForm';
-import QuizForm from '../../resources/Form/QuizForm';
-import EnvironmentForm from '../../resources/Form/EnvironmentForm';
+import FormFiller from '../../pages/FormFiller/FormFiller';
 import { AppContext } from '../../context/AppContext';
-import CustomTabs from '../../components/Tab/Tabs';
 import PageSwitcher from '../../components/PageSwitcher/PageSwitcher';
 import './Create.scss';
 
 class Create extends Component {
 	static contextType = AppContext;
-
-	decideForm = (type) => {
-		type = type.toLowerCase();
-		const props = {
-			user: this.props.user,
-			onSubmit: this.context.submitForm,
-			changeResponse: this.context.changeResponse
-		};
-		if (type === 'quiz') return <QuizForm {...props} />;
-		else if (type === 'question') return <QuestionForm {...props} />;
-		else if (type === 'folder') return <FolderForm {...props} />;
-		else if (type === 'environment') return <EnvironmentForm {...props} />;
-	};
-
 	render() {
 		return (
 			<PageSwitcher page="create">
 				{({ CustomTabs, type }) => (
 					<div className="Create page">
 						{CustomTabs}
-						{this.decideForm(type)}
+						<FormFiller
+							useModal={false}
+							user={this.props.user}
+							submitMsg={'Create'}
+							data={null}
+							type={type}
+							page={'Create'}
+							onSubmit={this.context.submitForm}
+						/>
 					</div>
 				)}
 			</PageSwitcher>
--- src/pages/FormFiller/FormFiller.jsx
+++ src/pages/FormFiller/FormFiller.jsx
@@ -29,39 +29,34 @@ class FormFiller extends Component {
 
 	decideForm = () => {
 		const { transformValue } = this;
-		const { data, user, submitMsg, onSubmit, page } = this.props;
+		const { data, user, submitMsg, onSubmit } = this.props;
+		const page = this.props.page.toLowerCase();
 		const type = this.props.type.toLowerCase();
 		const props = {
 			user,
 			submitMsg,
 			onSubmit,
-			customInputs: transformValue
+			customInputs: page !== 'create' ? transformValue : null
 		};
 		if (data) {
-			if (type === 'folder')
-				return <FolderForm {...props} selected_quizzes={page === 'Self' ? data.quizzes.map(({ _id }) => _id) : []} />;
-			else if (type === 'question')
-				return (
-					<QuestionForm
-						{...props}
-						defaultType={data.type}
-						selected_quiz={data.quiz ? data.quiz._id : ''}
-						defaultAnswers={data.answers}
-						blank_count={data.type === 'FIB' ? data.name.match(/\$\{_\}/g).length : 0}
-						defaultOptions={data.options}
-					/>
-				);
-			else if (type === 'quiz')
-				return (
-					<QuizForm
-						{...props}
-						tags={data.tags}
-						src={data.image}
-						selected_folders={page === 'Self' ? data.folders.map(({ _id }) => _id) : []}
-					/>
-				);
-			else if (type === 'environment') return <EnvironmentForm {...props} />;
-		} else return <div>N/A</div>;
+			if (type === 'folder') props.selected_quizzes = page === 'self' ? data.quizzes.map(({ _id }) => _id) : [];
+			else if (type === 'question') {
+				props.defaultType = data.type;
+				props.selected_quiz = data.quiz ? data.quiz._id : '';
+				props.defaultAnswers = data.answers;
+				props.blank_count = data.type === 'FIB' ? data.name.match(/\$\{_\}/g).length : 0;
+				props.defaultOptions = data.options;
+			} else if (type === 'quiz') {
+				props.tags = data.tags;
+				props.src = data.image;
+				props.selected_folders = page === 'self' ? data.folders.map(({ _id }) => _id) : [];
+			}
+		}
+
+		if (type === 'folder') return <FolderForm {...props} />;
+		else if (type === 'question') return <QuestionForm {...props} />;
+		else if (type === 'quiz') return <QuizForm {...props} />;
+		else if (type === 'environment') return <EnvironmentForm {...props} />;
 	};
 
 	render() {
