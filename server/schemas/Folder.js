8be8150e6d2bf4080f6a7569cdaef54db50d7490
Devon Reid <fireborn1003@gmail.com>
2020-06-23 19:36:52 +0600

    P.FT:API->Folder Added query operations

--- server/schemas/Folder.js
+++ server/schemas/Folder.js
@@ -1,7 +1,94 @@
 const { gql } = require('apollo-server-express');
 
 module.exports = gql`
-	type Folder {
+	interface Folder {
+		id: ID!
 		name: String!
+	}
+
+	type OthersFolder implements Folder {
+		id: ID!
+		name: String!
+	}
+
+	type SelfFolder implements Folder {
+		id: ID!
+		name: String!
+		public: Boolean!
+		favourite: Boolean!
+	}
+
+	extend type Query {
+    # All mixed
+    "Get all mixed folders (U)"
+		getAllMixedFolders: [OthersFolder!]!
+
+    "Get all mixed folders name and id (U)"
+		getAllMixedFoldersName: [NameAndId!]!
+
+    "Get all mixed folders count (U)"
+		getAllMixedFoldersCount: Int!
+
+    # All Others
+    "Get all other folders"
+		getAllOthersFolders: [OthersFolder!]!
+
+    "Get all other folders name and id"
+		getAllOthersFoldersName: [NameAndId!]!
+
+    "Get all others folders count"
+		getAllOthersFoldersCount: Int!
+
+    # All Self
+    "Get all self folders"
+		getAllSelfFolders: [SelfFolder!]!
+
+    "Get all self folders name and id"
+		getAllSelfFoldersName: [NameAndId!]!
+
+    "Get all self folders count"
+		getAllSelfFoldersCount: Int!
+
+    # Paginated mixed
+    "Get paginated mixed folders (U)"
+		getPaginatedMixedFolders(pagination: PaginationInput!): [OthersFolder!]!
+
+    "Get paginated mixed folders name and id (U)"
+		getPaginatedMixedFoldersName(pagination: PaginationInput!): [NameAndId!]!
+
+    "Get filtered mixed folders count (U)"
+    getFilteredMixedFoldersCount(filter: JSON): Int!
+
+    # Paginated others
+    "Get paginated others folders"
+		getPaginatedOthersFolders(pagination: PaginationInput!): [OthersFolder!]!
+
+    "Get paginated others folders name and id"
+		getPaginatedOthersFoldersName(pagination: PaginationInput!): [NameAndId!]!
+
+    "Get filtered others folders count"
+    getFilteredOthersFoldersCount(filter: JSON): Int!
+
+    # Paginated Self
+    "Get paginated self folders"
+		getPaginatedSelfFolders(pagination: PaginationInput!): [SelfFolder!]!
+
+    "Get paginated self folders name and id"
+		getPaginatedSelfFoldersName(pagination: PaginationInput!): [NameAndId!]!
+
+    "Get filtered self folders count"
+    getFilteredSelfFoldersCount(filter: JSON): Int!
+
+    # Id mixed
+    "Get mixed folder by id (U)"
+    getMixedFoldersById(id:ID!): OthersFolder!
+
+    # Id others
+    "Get others folder by id"
+    getOthersFoldersById(id: ID!): OthersFolder!
+
+    # Id self
+    "Get others folder by id"
+    getSelfFoldersById(id: ID!): SelfFolder!
 	}
 `;
