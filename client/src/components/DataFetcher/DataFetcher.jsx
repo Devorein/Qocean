67be6528d522699e004e267ee8497dc2b2211be3
Devon Reid <fireborn1003@gmail.com>
2020-06-28 17:54:55 +0600

    P.FT:Comp->DataFetcher Added data and count queries

--- client/src/components/DataFetcher/DataFetcher.jsx
+++ client/src/components/DataFetcher/DataFetcher.jsx
@@ -1,115 +1,42 @@
 import React from 'react';
-import axios from 'axios';
-import pluralize from 'pluralize';
-import qs from 'qs';
+import { connect } from 'react-redux';
+import { Query } from '@apollo/react-components';
+import Composer from 'react-composer';
 
 import { AppContext } from '../../context/AppContext';
-import populateQueryParams from '../../Utils/populateQueryParams';
 import detectCurrentEnvPageProp from '../../Utils/detectCurrentEnvPageProp';
 
 class DataFetcher extends React.Component {
 	static contextType = AppContext;
 	state = {
-		data: [],
-		totalCount: 0,
 		type: detectCurrentEnvPageProp({ user: this.context.user, page: this.props.page, prop: 'type' })
 	};
 
-	refetchData = (type, queryParams) => {
-		const page = this.props.page.toLowerCase();
-		type = type.toLowerCase();
-		populateQueryParams(type, queryParams, this.context.user, page);
-		const queryString = qs.stringify(queryParams, { depth: 10 });
-		const headers = {
-			headers: {
-				Authorization: `Bearer ${localStorage.getItem('token')}`
-			}
-		};
-
-		if (page.match(/(self|play)/)) {
-			axios
-				.get(`http://localhost:5001/api/v1/${pluralize(type, 2)}/countMine?${queryString}`, {
-					...headers
-				})
-				.then(({ data: { data: totalCount } }) => {
-					axios
-						.get(`http://localhost:5001/api/v1/${pluralize(type, 2)}/me?${queryString}`, {
-							...headers
-						})
-						.then(({ data: { data } }) => {
-							this.setState({
-								data,
-								totalCount,
-								type
-							});
-						});
-				})
-				.catch((err) => {
-					console.log(err);
-				});
-		} else if (page === 'explore') {
-			const [ count, endpoint, header ] = this.context.user
-				? [ `countOthers`, '/others/', headers ]
-				: [ 'countAll', '/', {} ];
-			if (type === 'question') {
-				axios
-					.post(`http://localhost:5001/api/v1/questions/others`, queryParams, {
-						headers: {
-							Authorization: `Bearer ${localStorage.getItem('token')}`
-						}
-					})
-					.then(({ data: { data, count } }) => {
-						this.setState({
-							data,
-							totalCount: count,
-							type
-						});
-					});
-			} else {
-				axios
-					.get(`http://localhost:5001/api/v1/${pluralize(type, 2)}/${count}?${queryString}`, { ...header })
-					.then(({ data: { data: totalCount } }) => {
-						axios
-							.get(`http://localhost:5001/api/v1/${pluralize(type, 2)}${endpoint}?${queryString}`, { ...header })
-							.then(({ data: { data } }) => {
-								this.setState({
-									data,
-									totalCount
-								});
-							});
-					})
-					.catch((err) => {
-						console.log(err);
-					});
-			}
-		} else if (page === 'watchlist') {
-			axios
-				.get(`http://localhost:5001/api/v1/watchlist/${pluralize(type, 2)}/count?${queryString}`, { ...headers })
-				.then(({ data: { data: totalCount } }) => {
-					axios
-						.get(`http://localhost:5001/api/v1/watchlist/${pluralize(type, 2)}?${queryString}`, { ...headers })
-						.then(({ data: { data } }) => {
-							this.setState({
-								data,
-								totalCount
-							});
-						});
-				})
-				.catch((err) => {
-					console.log(err);
-				});
-		}
-	};
-
 	render() {
-		const { data, totalCount, type } = this.state;
-		return this.props.children({
-			data,
-			totalCount,
-			refetchData: this.refetchData,
-			type
-		});
+		let { type } = this.state;
+		let { page, authedUser, pageQueries } = this.props;
+		page = page.toLowerCase();
+		type = type.toLowerCase();
+		const countQuery = pageQueries[`${page}.${type}.${authedUser ? 'auth' : 'unauth'}Count`];
+		const dataQuery = pageQueries[`${page}.${type}.${authedUser ? 'auth' : 'unauth'}`];
+		return countQuery && dataQuery ? (
+			<Composer
+				components={[
+					({ render }) => <Query query={countQuery} children={render} />,
+					({ render }) => <Query query={dataQuery} children={render} />
+				]}
+			>
+				{({ countQuery, dataQuery }) => {
+					return this.props.children({
+						data: dataQuery.data,
+						totalCount: countQuery.data,
+						type,
+						refetchData: dataQuery.refetch
+					});
+				}}
+			</Composer>
+		) : null;
 	}
 }
 
-export default DataFetcher;
+export default connect(({ authedUser, pageQueries }) => ({ authedUser, pageQueries }))(DataFetcher);
