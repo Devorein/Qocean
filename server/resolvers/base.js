9276f9dcd7c3dbdc06f01fd34bd90c14d047b770
Devon Reid <fireborn1003@gmail.com>
2020-07-18 23:20:50 +0600

    Added mongql integration

--- server/resolvers/base.js
+++ server/resolvers/base.js
@@ -1,9 +1,4 @@
-const Password = require('../types/password');
-const Username = require('../types/username');
-
 module.exports = {
-	Password,
-	Username,
 	Query: {},
 	Mutation: {}
 };
--- server/resource.js
+++ server/resource.js
@@ -1,5 +1,4 @@
 const S = require('string');
-const { typeDefs: ExternalTypeDef, resolvers: ExternalResolvers } = require('graphql-scalars');
 const Mongql = require('mongql');
 
 const modelschema = require('./models');
@@ -9,24 +8,19 @@ const routes = require('./routes');
 const AuthTypedef = require('./typedefs/Auth');
 const AuthResolvers = require('./resolvers/auth');
 
-const BaseTypedef = require('./typedefs/Base');
-const BaseResolvers = require('./resolvers/base');
-
 const PreTransformedTypeDefsASTs = require('./typedefs');
 const PreTransformedResolvers = require('./resolvers');
 
-const ModelsArr = [];
-const ModelsObj = {};
 const SchemasArr = [];
 const SchemasObj = {};
 
-Object.entries(modelschema).forEach(([ resource, [ model, schema ] ]) => {
-	ModelsObj[S(resource).capitalize().s] = model;
-	ModelsArr.push(model);
-	SchemasObj[S(resource).capitalize().s] = schema;
-	SchemasArr.push(schema);
+Object.entries(modelschema).forEach(([ resource, value ]) => {
+	SchemasObj[S(resource).capitalize().s] = value[1];
+	SchemasArr.push(value[1]);
 });
+
 const RoutesArr = [];
+
 Object.values(routes).forEach((route) => {
 	RoutesArr.push(route);
 });
@@ -45,37 +39,13 @@ module.exports = {
 				dir: process.cwd() + '\\SDL'
 			}
 		});
-
-		const { TransformedResolvers, TransformedTypedefs } = await mongql.generate();
-		const TypedefsArr = TransformedTypedefs.arr;
-		const TypedefsObj = TransformedTypedefs.obj;
-		const ResolversArr = TransformedResolvers.arr;
-		const ResolversObj = TransformedResolvers.obj;
-		[
-			[ 'Auth', AuthTypedef, AuthResolvers ],
-			[ 'Base', BaseTypedef, BaseResolvers ],
-			[ 'External', ExternalTypeDef, ExternalResolvers ]
-		].forEach(([ key, typedef, resolver ]) => {
-			TypedefsObj[key] = typedef;
-			if (Array.isArray(typedef)) TypedefsArr.push(...typedef);
-			else TypedefsArr.push(typedef);
-			ResolversObj[key] = resolver;
-			ResolversArr.push(resolver);
-		});
-		return {
-			Typedefs: {
-				obj: TypedefsObj,
-				arr: TypedefsArr
-			},
-			Resolvers: {
-				obj: ResolversObj,
-				arr: ResolversArr
-			}
-		};
-	},
-	Models: {
-		obj: ModelsObj,
-		arr: ModelsArr
+		const { TransformedTypedefs, TransformedResolvers } = await mongql.generate();
+		TransformedTypedefs.obj.Auth = AuthTypedef;
+		TransformedTypedefs.arr.push(AuthTypedef);
+		TransformedResolvers.obj.Auth = AuthResolvers;
+		TransformedResolvers.arr.push(AuthResolvers);
+		const generatedModels = await mongql.generateModels();
+		return { TransformedTypedefs, TransformedResolvers, generatedModels };
 	},
 	Routes: {
 		obj: routes,
